fdde84035e24f00ed06550861d9fc55095c2de75
valeo.popov <valeo.popov@gmail.com>
2020-01-04 09:08:48 +0300

    fix initial position of game window

--- desktop/src/com/mygdx/obstacleavoid/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/obstacleavoid/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.mygdx.obstacleavoid.ObstacleAvoidGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		setInitialPosition(config);
 		new LwjglApplication(new ObstacleAvoidGame(), config);
+	}
+
+	private static void setInitialPosition(LwjglApplicationConfiguration config) {
+		config.x = 100;
+		config.y = 100;
 	}
 }
