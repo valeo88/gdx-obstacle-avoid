f70c921572dfb12c7c1d535fcbdf47a50b69f758
valeo.popov <valeo.popov@gmail.com>
2020-01-05 18:29:36 +0300

    using JSON config for camera controller debug

--- core/src/com/mygdx/obstacleavoid/util/debug/DebugCameraController.java
+++ core/src/com/mygdx/obstacleavoid/util/debug/DebugCameraController.java
@@ -2,7 +2,6 @@ package com.mygdx.obstacleavoid.util.debug;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -11,27 +10,14 @@ import com.badlogic.gdx.utils.Logger;
 public class DebugCameraController {
     private static final Logger log = new Logger(DebugCameraController.class.getName(), Logger.DEBUG);
 
-    private static final int DEFAULT_LEFT_KEY = Input.Keys.A;
-    private static final int DEFAULT_RIGHT_KEY = Input.Keys.D;
-    private static final int DEFAULT_UP_KEY = Input.Keys.W;
-    private static final int DEFAULT_DOWN_KEY = Input.Keys.S;
-
-    private static final int DEFAULT_ZOOM_IN_KEY = Input.Keys.COMMA;
-    private static final int DEFAULT_ZOOM_OUT_KEY = Input.Keys.PERIOD;
-
-    private static final int DEFAULT_RESET_KEY = Input.Keys.BACKSPACE;
-    private static final int DEFAULT_LOG_KEY = Input.Keys.ENTER;
-
-    private static final float DEFAULT_MOVE_SPEED = 20f;
-    private static final float DEFAULT_ZOOM_SPEED = 2.0f;
-    private static final float DEFAULT_MAX_ZOOM_IN = 0.20f;
-    private static final float DEFAULT_MAX_ZOOM_OUT = 30f;
-
     private Vector2 position = new Vector2();
     private Vector2 startPosition = new Vector2();
     private float zoom = 1.0f;
+    private final DebugCameraConfig config;
 
     public DebugCameraController() {
+        config = new DebugCameraConfig();
+        log.debug("config=" + config);
     }
 
     public void setStartPosition(float x, float y) {
@@ -51,34 +37,34 @@ public class DebugCameraController {
             return;
         }
 
-        float moveSpeed = DEFAULT_MOVE_SPEED * delta;
-        float zoomSpeed = DEFAULT_ZOOM_SPEED * delta;
+        float moveSpeed = config.getMoveSpeed() * delta;
+        float zoomSpeed = config.getZoomSpeed() * delta;
 
         // move controls
-        if (Gdx.input.isKeyPressed(DEFAULT_LEFT_KEY)) {
+        if (config.isLeftPressed()) {
             moveLeft(moveSpeed);
-        } else if (Gdx.input.isKeyPressed(DEFAULT_RIGHT_KEY)) {
+        } else if (config.isRightPressed()) {
             moveRight(moveSpeed);
-        } else if (Gdx.input.isKeyPressed(DEFAULT_UP_KEY)) {
+        } else if (config.isUpPressed()) {
             moveUp(moveSpeed);
-        } else if (Gdx.input.isKeyPressed(DEFAULT_DOWN_KEY)) {
+        } else if (config.isDownPressed()) {
             moveDown(moveSpeed);
         }
 
         // zoom controls
-        if (Gdx.input.isKeyPressed(DEFAULT_ZOOM_IN_KEY)) {
+        if (config.isZoomInPressed()) {
             zoomIn(zoomSpeed);
-        } else if (Gdx.input.isKeyPressed(DEFAULT_ZOOM_OUT_KEY)) {
+        } else if (config.isZoomOutPressed()) {
             zoomOut(zoomSpeed);
         }
 
         // reset controls
-        if (Gdx.input.isKeyPressed(DEFAULT_RESET_KEY)) {
+        if (config.isResetPressed()) {
             reset();
         }
 
         // log controls
-        if (Gdx.input.isKeyPressed(DEFAULT_LOG_KEY)) {
+        if (config.isLogPressed()) {
             logDebug();
         }
     }
@@ -88,7 +74,7 @@ public class DebugCameraController {
     }
 
     private void setZoom(float value) {
-        zoom = MathUtils.clamp(value, DEFAULT_MAX_ZOOM_IN, DEFAULT_MAX_ZOOM_OUT);
+        zoom = MathUtils.clamp(value, config.getMaxZoomIn(), config.getMaxZoomOut());
     }
 
     private void moveCamera(float xSpeed, float ySpeed) {
