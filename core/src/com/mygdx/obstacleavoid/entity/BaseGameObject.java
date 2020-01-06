cfcc1cd0ba2bc07e6d83e4462b796eaa51691e06
valeo.popov <valeo.popov@gmail.com>
2020-01-06 10:03:12 +0300

    BaseGameObject as abstract parent for Player and Obstacle

---
+++ core/src/com/mygdx/obstacleavoid/entity/BaseGameObject.java
@@ -0,0 +1,47 @@
+package com.mygdx.obstacleavoid.entity;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+
+public abstract class BaseGameObject {
+
+    private float x;
+    private float y;
+    private Circle bounds;
+
+    public BaseGameObject(float boundsRaduis) {
+        bounds = new Circle(x, y, boundsRaduis);
+    }
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+        updateBounds();
+    }
+
+    public void setX(float x) {
+        this.x = x;
+        updateBounds();
+    }
+
+    public void setY(float y) {
+        this.y = y;
+        updateBounds();
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void updateBounds() {
+        bounds.setPosition(x, y);
+    }
+
+    public void drawDebug(ShapeRenderer renderer) {
+        renderer.circle(bounds.x, bounds.y, bounds.radius, 30);
+    }
+}
--- core/src/com/mygdx/obstacleavoid/entity/Obstacle.java
+++ core/src/com/mygdx/obstacleavoid/entity/Obstacle.java
@@ -1,49 +1,20 @@
 package com.mygdx.obstacleavoid.entity;
 
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Circle;
 
-public class Obstacle {
+public class Obstacle extends  BaseGameObject{
 
     private static final float BOUNDS_RADIUS = 0.3f;
     private static final float SIZE = 2 * BOUNDS_RADIUS;
 
-    private float x;
-    private float y;
-
     private float ySpeed = 0.1f;
 
-    private Circle bounds;
-
     public Obstacle() {
-        bounds = new Circle(x, y, BOUNDS_RADIUS);
+        super(BOUNDS_RADIUS);
     }
 
-    public void setPosition(float x, float y) {
-        this.x = x;
-        this.y = y;
-        updateBounds();
-    }
 
     public void update() {
-        setPosition(x, y - ySpeed);
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    private void updateBounds() {
-        bounds.setPosition(x, y);
-    }
-
-    /** In debug mode represent obstacle as circle. */
-    public void drawDebug(ShapeRenderer renderer) {
-        renderer.circle(bounds.x, bounds.y, bounds.radius, 30);
+        setY(getY() - ySpeed);
     }
 
     public float getWidth() {
